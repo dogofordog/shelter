21fa555345e00892368fc92f1db1b18151685935
github-username <118908628+dogofordog@users.noreply.github.com>
2026-06-28 20:43:04 +0300

    feat: implement infinite carousel with unique pets per group Sun, Jun 28, 2026 08:43:04 PM

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,15 +1,10 @@
+function shuffle(array) {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+}
 
-// function shuffle(array) {
-//   const arr = [...array];
-//   for (let i = arr.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [arr[i], arr[j]] = [arr[j], arr[i]];
-//   }
-//   return arr;
-// }
-
-// function randomFrom(array) {
-//   return array[Math.floor(Math.random() * array.length)];
-// }
-
-// export { shuffle, randomFrom };
+export { shuffle };
